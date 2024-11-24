7f962f70c78d3e870ea57e0b89bd742165d18389
mrfish233 <baboom003@gmail.com>
2024-11-24 21:58:23 +0800

    feat: add student conversation with Groq API

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,36 @@
 import express from 'express';
+import Groq from 'groq-sdk';
 
 let app = express();
+const port = 8081;
 
 app.use(express.static('www'));
+app.use(express.json());
 
-let server = app.listen(8081, function () {
-    let host = server.address().address;
-    let port = server.address().port;
+app.post('/student_conversation', async function (req, res) {
+    const msg = req.body.message;
+    const key = process.env.GROQ_API_KEY;
 
-    console.log("Express listening at http://%s:%s", host, port);
+    const groq = new Groq({ apiKey: key });
+    const response = await groq.chat.completions.create({
+        messages: [
+            {
+                role: 'system',
+                content: 'You are a helpful assistant that always use chinese to communicate with the user.',
+            },
+            {
+                role: 'user',
+                content: msg,
+            },
+        ],
+        model: "llama3-8b-8192"
+    });
+
+    console.log(response.choices[0]?.message?.content || 'No response');
+
+    res.send(response);
+});
+
+app.listen(port, function () {
+    console.log(`App listening on port ${port}`);
 });
