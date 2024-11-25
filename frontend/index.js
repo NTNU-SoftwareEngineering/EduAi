8161d1b82a56d5589a4c2dca956c1e1633f575d9
mrfish233 <baboom003@gmail.com>
2024-11-25 14:06:22 +0800

    refactor: change frontend directory name

--- frontend/index.js
+++ frontend/index.js
@@ -4,12 +4,21 @@ import Groq from 'groq-sdk';
 let app = express();
 const port = 8081;
 
-app.use(express.static('www'));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/student_conversation', async function (req, res) {
     const msg = req.body.message;
     const key = process.env.GROQ_API_KEY;
+
+    if (msg === undefined) {
+        res.status(400).send('Bad request');
+        return;
+    }
+    else if (key === undefined) {
+        res.status(500).send('Internal server error');
+        return;
+    }
 
     const groq = new Groq({ apiKey: key });
     const response = await groq.chat.completions.create({
